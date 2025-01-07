c8564cb603894e14859dca1be9755e0cf9fb901a
Swastik Ranjan <swastik.ranjan@hines.com>
2025-01-07 19:01:02 +0530

    added django

---
+++ backendd/api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User 
+
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+     
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        # return super().get_queryset()
